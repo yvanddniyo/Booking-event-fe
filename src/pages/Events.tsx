cb34ba9efae990c12ccd3dbbae58c1fa8ba6937b
yvanddniyo <nyvan80076@gmail.com>
2025-07-21 02:00:22 +0200

    adding search functionality on the event.

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import EventCard from "../components/EventPage/EventCard";
 import EventSideBar from "../components/EventPage/EventSideBar";
 import CardLoaderSkeleton from "../components/Skeleton/CardLoaderSkeleton";
@@ -5,25 +6,28 @@ import { useGetEvents } from "../hooks/useRegisterAndLogin";
 import type { Event } from "../type";
 
 const Events = () => {
-  const { data, isLoading } = useGetEvents();
-  console.log("data -->", data);
+  const [search, setSearch ] = useState("");
+  const { data, isLoading} = useGetEvents();
+  const filteredEvents = data?.data?.filter((event: Event) => event.title.toLowerCase().includes(search.toLowerCase()));
   return (
     <div className="flex flex-col md:flex-row gap-6 p-6">
       <EventSideBar />
       <div className="flex-1 overflow-y-auto">
         <div className="mb-6">
-          <input type="text" placeholder="Search events" className="w-full border border-gray-300 rounded-full p-2 outline-none px-4" />
+          <input type="text" placeholder="Search events" className="w-full border border-gray-300 rounded-full p-2 outline-none px-4" value={search} onChange={(e) => setSearch(e.target.value)} />
         </div>
         
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {isLoading ? Array.from({ length: 6 }).map((_, index) => (
             <CardLoaderSkeleton key={index} />
-          )) : data?.data?.map((event: Event) => (
+          )) : filteredEvents?.length > 0 ? filteredEvents?.map((event: Event) => (
             <EventCard
               key={event.id}
               event={event}
             />
-          ))}
+          )) : <div className="col-span-full text-center py-8">
+            No events found
+          </div>}
         </div>
       </div>
     </div>
